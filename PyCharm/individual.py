ccd85b45db4502336b6d3c64952052695732e6cc
internetHacker1123 <dinozavrik9911@mail.ru>
2023-12-08 19:41:11 +0300

    add individual task

---
+++ PyCharm/individual.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    sp = []
+
+    while True:
+        inp = input(">>> ").lower()
+
+        if inp == 'add':
+            punkt_naznachenia = input("Пункт назначения поезда: ")
+            train_number = input("Номер поезда: ")
+            time_otpravlenia = input("Время отправления: ")
+
+            dictionary = {
+                'Пункт назначения ': punkt_naznachenia,
+                'Номер поезда: ': train_number,
+                'Время отправления:': time_otpravlenia
+            }
+
+            sp.append(dictionary)
+            sp = sorted(sp, key=lambda x: x['Номер поезда: '])
+
+        
+        if inp.isdigit():
+            for d in sp:
+                if inp in d.values():
+                    print(d)
+                else: 
+                    print('Поезда с таким номером нет')
+
