a1a210f555d8d4a2a60e763eb79015ee7c5f91ce
internetHacker1123 <dinozavrik9911@mail.ru>
2023-12-12 19:29:40 +0300

    dodelal

--- PyCharm/individual.py
+++ PyCharm/individual.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import re
 
 if __name__ == '__main__':
     sp = []
 
     while True:
         inp = input(">>> ").lower()
-
         match inp:
             case 'add':
                 punkt_naznachenia = input("Пункт назначения поезда: ")
@@ -23,10 +23,48 @@ if __name__ == '__main__':
                 sp = sorted(sp, key=lambda x: x['Номер поезда: '])
 
             
-            case inp.isdigit():
+            case 'choose':
+                inp = input("Введите номер поезда: ")
                 for d in sp:
                     if inp in d.values():
                         print(d)
                     else: 
                         print('Поезда с таким номером нет')
+            
+            case 'list':
+                line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+               '-' * 4,
+               '-' * 30,
+               '-' * 20,
+               '-' * 20
+                )
+                print(line)
+                print(
+               '| {:^4} | {:^30} | {:^20} | {:^20} |'.format(
+                   "№",
+                   "Пункт назначения",
+                   "Номер поезда",
+                   "Время отправления"
+                )
+                )
+                print(line)
+                for idx, train in enumerate(sp, 1):
+                    print(
+                        '| {:>4} | {:<30} | {:<20} | {:>20} |'.format(
+                            idx,
+                            train.get('Пункт назначения ', ''),
+                            train.get('Номер поезда: ', ''),
+                            train.get('Время отправления:', 0)
+                   )
+               )
+                print(line)
+            case 'exit':
+                break
+
+            case 'help':
+                print("Список команд:\n")
+                print("add - добавить поезд;")
+                print("list - вывести список поездов;")
+                print("help - отобразить справку;")
+                print("exit - завершить работу с программой.")
 
